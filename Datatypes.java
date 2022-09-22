3735f7283ea74e4dd84293f197ed66b9a945d3f4
swaruchandu222 <swaru.chandu222@gmail.com>
2022-09-22 15:26:03 +0530

    selenium

---
+++ Datatypes.java
@@ -0,0 +1,28 @@
+package javabasics;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a;
+int b=10;
+a=20;
+char c='@';
+float f=12.34f;
+double d=123.456;
+
+System.out.println("value of f is "+f);
+System.out.println("value of d is "+d);
+System.out.println("value of c is "+c);
+String st="test";
+System.out.println("value of a is "+a);
+System.out.println("value of b is "+b);
+System.out.println(a+b);
+System.out.println(c+a);
+System.out.println(c+a+b);
+System.out.println(st+a);
+System.out.println(st+a+b);
+System.out.println(a++);
+	}
+
+}
